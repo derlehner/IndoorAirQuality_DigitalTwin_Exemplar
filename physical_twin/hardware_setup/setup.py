393b748814fef5763b2b01a92ef81c29008df1a2
Daniel Lehner <derlehner@users.noreply.github.com>
2021-09-03 13:33:58 +0200

    Delete setup.py

--- physical_twin/hardware_setup/setup.py
+++
@@ -1,9 +0,0 @@
-import test
-import urllib3
-urllib3.disable_warnings()
-
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-test.create_schema()
-test.create_instances()
-urllib3.disable_warnings()
