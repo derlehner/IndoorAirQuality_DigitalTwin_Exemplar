bc98f354efc911d0ea6debd70dcaf9a7652a6df7
hari <harishankarghs@gmail.com>
2021-09-16 16:30:13 +0200

    update on automation

--- physical_twin/hardware_setup/auto_deploy_script.py
+++ physical_twin/hardware_setup/auto_deploy_script.py
@@ -39,7 +39,7 @@ def checking_dir_if_already_exitst(source, ip_ad, username, password, device_nam
     copying_file_to_raspberry(source, ip_ad)   # copying file/dir From: 'source' as path To: /home/pi/ path in all raspberry
     ssh_conn(cmd= 'rm -Rf {}'.format(device_name), ip_ad=ip_ad, username=username, password=password) # Deleting old device.txt file if already exist
     ssh_conn(cmd= 'echo {} > /home/pi/device_id.txt'.format(device_name), ip_ad=ip_ad, username=username, password=password) # Creating new device_name.txt for device name
-    ssh_conn(cmd= 'python3 test_scd30.py &', ip_ad=ip_ad, username=username, password=password) # to kill the bg process kill $(jobs -p)
+    ssh_conn(cmd= 'python3 {}'.format(sys.argv[1]), ip_ad=ip_ad, username=username, password=password) # to kill the bg process kill $(jobs -p)
     return None
 
 
