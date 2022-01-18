a7e552a1a40ab7b5dab85e628467ac40705d9ca5
Hari Shankar <43772343+Harighs@users.noreply.github.com>
2022-01-18 10:49:05 +0100

    Update AlarmService.py
    Update of Alarm and LED

--- physical_twin/hardware_setup/Improved_Architecture/AlarmService.py
+++ physical_twin/hardware_setup/Improved_Architecture/AlarmService.py
@@ -1,10 +1,26 @@
 from fastapi import Body, FastAPI
 
+import RPi.GPIO as GPIO # IMPORTANT: remember to change the gpio pin (18) also it needs to be programmed in Thonny Python IDE
+import time #used in raspberry pi model 4
+
+
+
 app = FastAPI()
+
 
 class AlarmService:
 
+    def __init__(self) -> None:
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM) 
+        GPIO.setup(18, GPIO.OUT)
+        pass
+
     @app.post("set_alarm")
     def set_alarm(value: bool = Body(...)):
-        # TODO: put code for setting/desetting LED light here
+        if () # Condition
+        GPIO.output(18, True) 
+        # For alarm: make series connectioin of beeper along with this  LED in the breadboard
+        else () # else condition
+        GPIO.output(18, False) 
         pass
